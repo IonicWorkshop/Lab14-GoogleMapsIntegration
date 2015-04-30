3b6661fb9da00083624e7a9df42b30ae911d5dd1
digitaldrummerj@gmail.com <digitaldrummerj@gmail.com>
2015-04-29 20:45:02 -0700

    setup ionic sass, added google map.  pin not working yet.  showing map for zip code

--- www/js/ContactDetailsCtrl.js
+++ www/js/ContactDetailsCtrl.js
@@ -1,6 +1,34 @@
 angular.module('starter')
-    .controller('ContactDetailsCtrl', function ($scope, $stateParams, ContactsService) {
+    .controller('ContactDetailsCtrl',
+        function ($scope,
+            $stateParams,
+            ContactsService,
+            uiGmapIsReady,
+            uiGmapGoogleMapApi,
+            GeolocationService,
+            $timeout)
+        {
         var index = $stateParams.index;
 
         $scope.contact = ContactsService.GetContact(index);
+            $scope.map = {
+                zoom: 15,
+                center: GeolocationService.getDefaultPosition(),
+                control: {}
+            };
+
+            // Main map object for this view
+        uiGmapGoogleMapApi.then(function(maps) {
+            var address = $scope.contact.user.location.zip;
+            console.log(address);
+
+            GeolocationService.addressToPosition(address).then(function(coordinates) {
+                console.log(coordinates);
+                if (coordinates.latitude && coordinates.longitude) {
+                    console.log('center and refresh');
+                    $scope.map.center = coordinates;
+                    $scope.map.control.refresh(coordinates);
+                }
+            });
+        });
     });
